b7f9dd6691a691cc67f323dc8be1f9fe5ff2bfc3
Quinten Lansu <quintenlansu@gmail.com>
2017-11-18 14:51:46 +0100

    Add camera controller.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject m_titan;
+    public Vector3 m_offset = new Vector3(0.0f, 0.0f, -10.0f);
+
+    void Start()
+    {
+        m_offset = transform.position - m_titan.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = m_titan.transform.position + m_offset;
+    }
+}
