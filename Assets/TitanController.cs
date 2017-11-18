6dd5e519168945c39e9d9624a2a40f7956acb854
Quinten Lansu <quintenlansu@gmail.com>
2017-11-18 18:31:01 +0100

    Add health and balance energy somewhat.

--- Assets/TitanController.cs
+++ Assets/TitanController.cs
@@ -8,36 +8,41 @@ public class TitanController : MonoBehaviour {
     public GameObject m_prefabWood;
     public GameObject m_prefabFood;
     public GameObject m_textEnergy;
+    public GameObject m_textHealth;
     public GameObject m_textFood;
     public GameObject m_textWood;
     public float m_movementSpeed = 3.0f;
     public float m_energy = 100.0f;
+    public float m_health = 100.0f;
     public float m_wood = 0.0f;
     public float m_food = 0.0f;
 
     private GameObject m_miniGame;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Update ()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * m_movementSpeed * Time.deltaTime;
             m_energy -= 0.1f;
+            if (m_energy <= 0.0f)
+            {
+                m_health -= 0.1f;
+            }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (
+            Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * m_movementSpeed * Time.deltaTime;
             m_energy -= 0.1f;
+            if (m_energy <= 0.0f)
+            {
+                m_health -= 0.1f;
+            }
         }
         else
         {
-            m_energy += 0.025f;
+            m_energy += 0.1f;
         }
     }
 
@@ -45,6 +50,9 @@ public class TitanController : MonoBehaviour {
     {
         m_energy = Mathf.Clamp(m_energy, 0.0f, 100.0f);
         m_textEnergy.GetComponent<Text>().text = "Energy: " + m_energy.ToString("f0");
+
+        m_health = Mathf.Clamp(m_health, 0.0f, 100.0f);
+        m_textHealth.GetComponent<Text>().text = "Health: " + m_health.ToString("f0");
 
         m_wood = Mathf.Clamp(m_wood, 0.0f, 100.0f);
         m_textFood.GetComponent<Text>().text = "Wood: " + m_wood.ToString("f0");
