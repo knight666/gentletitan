c3af3f42ff74b07a032bbcea8a3abe955ce08852
Gerd <g.fankhaenel@gmail.com>
2017-11-18 13:57:41 +0100

    Add controller for titan.

---
+++ Assets/TitanController.cs
@@ -0,0 +1,27 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitanController : MonoBehaviour {
+
+    public float m_movementSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * m_movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * m_movementSpeed * Time.deltaTime;
+        }
+    }
+}
